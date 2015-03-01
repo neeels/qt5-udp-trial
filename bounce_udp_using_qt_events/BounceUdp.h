ae1cd2f7c1c0282a5a9ab6b13a1a05723c390939
Neels Hofmeyr <neels@hofmeyr.de>
2015-03-01 18:03:44 +0100

    bounce_udp_using_qt_events: comment

--- bounce_udp_using_qt_events/BounceUdp.h
+++ bounce_udp_using_qt_events/BounceUdp.h
@@ -10,12 +10,24 @@ class BounceUdp : public QObject
 
   public:
     
+    /*! \param out  Print any log output to this stream.
+        \param listenPort  Port number to listen on (on "any" localhost IPs).
+     */
     BounceUdp(QTextStream *out, int listenPort, QObject *parent=0);
 
+    /*! Open a socket. Return false if binding to the socket failed.
+        The socket is managed internally.
+     */ 
     bool bind();
+
+    /*! Close a previously opened socket. */
     void close();
 
   private slots:
+    /*! Receive any datagrams available from the bound socket, and
+        resend them according to the hardcoded internal rules.
+        bind() must have been called successfully.
+     */
     void receiveDatagram();
 
   private:
