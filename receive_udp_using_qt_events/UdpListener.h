5669193127cb94d405c2f7105e8b093b4b20d228
Neels Hofmeyr <neels@hofmeyr.de>
2015-03-01 18:04:11 +0100

    receive_udp_using_qt_events: comment

--- receive_udp_using_qt_events/UdpListener.h
+++ receive_udp_using_qt_events/UdpListener.h
@@ -10,12 +10,23 @@ class UdpListener : public QObject
 
   public:
     
+    /*! \param out  Print any log output to this stream.
+        \param port  Port number to listen on (on "any" localhost IPs).
+     */
     UdpListener(QTextStream *out, int port, QObject *parent=0);
 
+    /*! Open a socket. Return false if binding to the socket failed.
+        The socket is managed internally.
+     */ 
     bool bind();
+
+    /*! Close a previously opened socket. */
     void close();
 
   private slots:
+    /*! Receive any datagrams available from the bound socket, and
+        print their content on the out textstream passed to the constructor.
+     */
     void receiveDatagram();
 
   private:
