5406d3de016c87c9029412fcb30cec8b3d093992
Neels Hofmeyr <neels@hofmeyr.de>
2015-03-08 20:56:24 +0100

    bounce_udp_using_qt_events: use QOsQuit

--- bounce_udp_using_qt_events/main.cpp
+++ bounce_udp_using_qt_events/main.cpp
@@ -11,34 +11,12 @@
 #include <QCoreApplication>
 #include <QtCore>
 #include <qiostream.h>
-#include <QOsSignalReceiver>
+#include <QOsQuit>
 #include "BounceUdp.h"
-
-class RecvSignals : public QOsSignalReceiver
-{
-  public:
-    RecvSignals(QCoreApplication *core) : QOsSignalReceiver(core)
-    {
-      this->core = core;
-    }
-
-    virtual void receiveOsSignal(int signal)
-    {
-      qout << "\nSignal received: '" << strsignal(signal) << "'." << endl;
-      QTimer::singleShot(0, core, SLOT(quit()));
-    }
-
-  private:
-    QCoreApplication *core;
-};
 
 int main(int argc, char *argv[]) {
   QCoreApplication a(argc, argv);
-
-  RecvSignals *recvSignals = new RecvSignals(&a);
-  recvSignals->handleOsSignal(SIGINT);
-  recvSignals->handleOsSignal(SIGQUIT);
-  recvSignals->handleOsSignal(SIGTERM);
+  QOsQuit signalCatcher(a);
 
   int port = 1337;
 
