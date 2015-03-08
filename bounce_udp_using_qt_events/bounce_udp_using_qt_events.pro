146a67de0d08f82d8b7fe918b231a9f986c858a8
Neels Hofmeyr <neels@hofmeyr.de>
2015-03-08 18:52:33 +0100

    bounce_udp_using_qt_events: use QOsSignalReceiver to catch SIGINT

--- bounce_udp_using_qt_events/bounce_udp_using_qt_events.pro
+++ bounce_udp_using_qt_events/bounce_udp_using_qt_events.pro
@@ -5,6 +5,9 @@ TARGET = bounce_udp
 INCLUDEPATH += ../qiostream
 LIBS += -L../qiostream -lqiostream
 
+INCLUDEPATH += ../qossignalreceiver
+LIBS += -L../qossignalreceiver -lqossignalreceiver
+
 HEADERS += BounceUdp.h
 SOURCES += BounceUdp.cpp
 
--- bounce_udp_using_qt_events/main.cpp
+++ bounce_udp_using_qt_events/main.cpp
@@ -64,12 +64,38 @@
 
 */
 
+#include <string.h>
 #include <QCoreApplication>
+#include <QtCore>
 #include <qiostream.h>
+#include <QOsSignalReceiver>
 #include "BounceUdp.h"
+
+class RecvSignals : public QOsSignalReceiver
+{
+  public:
+    RecvSignals(QCoreApplication *core) : QOsSignalReceiver(core)
+    {
+      this->core = core;
+    }
+
+    virtual void receiveOsSignal(int signal)
+    {
+      qout << "\n" << strsignal(signal) << " signal received." << endl;
+      QTimer::singleShot(0, core, SLOT(quit()));
+    }
+
+  private:
+    QCoreApplication *core;
+};
 
 int main(int argc, char *argv[]) {
   QCoreApplication a(argc, argv);
+
+  RecvSignals *recvSignals = new RecvSignals(&a);
+  recvSignals->handleOsSignal(SIGINT);
+  recvSignals->handleOsSignal(SIGQUIT);
+  recvSignals->handleOsSignal(SIGTERM);
 
   int port = 1337;
 
